5ab8db78a76683f42d775b46c9e2cd2572a742fe
sangeetha <77095125+SangeethaKunche@users.noreply.github.com>
2023-04-04 22:24:29 +0530

    try catch added

--- src/demo2package/trycatchdemo.java
+++ src/demo2package/trycatchdemo.java
@@ -1,4 +1,13 @@
 package demo2package;
 
 public class trycatchdemo {
+    int divideNumber(int div, int num){
+        try{
+            int result = num/div;
+            return result;
+        }catch (Exception e){
+System.out.println("Divided by zero");
+        }
+return  0;
+    }
 }
